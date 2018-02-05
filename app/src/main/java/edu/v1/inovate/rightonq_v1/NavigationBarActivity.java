04343984efe76aa04f553e896d290d3913c409a4
katherine-07 <luisa_gilig@dlsu.edu.ph>
2018-02-05 17:55:43 +0800

    With Navigation Bar

---
+++ app/src/main/java/edu/v1/inovate/rightonq_v1/NavigationBarActivity.java
@@ -0,0 +1,20 @@
+package edu.v1.inovate.rightonq_v1;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class NavigationBarActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_navigation_bar);
+    }
+}
